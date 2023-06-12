5f0e47acc904aef748a0c5f35471dc4101e35bca
곽의진 <yuijin0909@gmail.com>
2023-06-12 23:52:12 +0900

    First commit

---
+++ app/src/main/java/com/example/camera/CameraExampleApplication.kt
@@ -0,0 +1,14 @@
+package com.example.camera
+
+import android.app.Application
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraXConfig
+
+class CameraExampleApplication: Application(), CameraXConfig.Provider {
+
+    override fun getCameraXConfig(): CameraXConfig = Camera2Config.defaultConfig()
+
+
+
+
+}
